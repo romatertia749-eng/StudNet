b11927a3a1a4c28324efdd6866b868eb6596afa7
unknown <romatertia749@gmail.com>
2025-12-06 18:47:18 +0300

    _

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic',
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -23,5 +25,8 @@ export default defineConfig({
     },
   },
   publicDir: 'public',
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
 });
 
